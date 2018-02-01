de0cb6bc6089405f479a17683579fd00af0e4ee4
kabocha23 <jekobuchi@gmail.com>
2018-02-01 09:07:38 -0800

    Added prompt

--- checkPalindrome.js
+++ checkPalindrome.js
@@ -1,3 +1,28 @@
+// Given the string, check if it is a palindrome.
+
+// Example
+
+// For inputString = "aabaa", the output should be
+// checkPalindrome(inputString) = true;
+// For inputString = "abac", the output should be
+// checkPalindrome(inputString) = false;
+// For inputString = "a", the output should be
+// checkPalindrome(inputString) = true.
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] string inputString
+
+// A non-empty string consisting of lowercase characters.
+
+// Guaranteed constraints:
+// 1 ≤ inputString.length ≤ 105.
+
+// [output] boolean
+
+// true if inputString is a palindrome, false otherwise.
+
 function checkPalindrome(inputString) {
     for(var a = 0; a < inputString.length/2; a++){
        for(var z = inputString.length-1; z > a; z--){
