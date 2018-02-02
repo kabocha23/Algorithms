07be531f9c23a200c9eeea2c02bf5ab2b67c52da
kabocha23 <jekobuchi@gmail.com>
2018-02-02 12:58:43 -0800

    Added Prompt

--- commonCharacterCount.js
+++ commonCharacterCount.js
@@ -1,3 +1,32 @@
+// Given two strings, find the number of common characters between them.
+
+// Example
+
+// For s1 = "aabcc" and s2 = "adcaa", the output should be
+// commonCharacterCount(s1, s2) = 3.
+
+// Strings have 3 common characters - 2 "a"s and 1 "c".
+
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] string s1
+
+// A string consisting of lowercase latin letters a-z.
+
+// Guaranteed constraints:
+// 1 ≤ s1.length ≤ 15.
+
+// [input] string s2
+
+// A string consisting of lowercase latin letters a-z.
+
+// Guaranteed constraints:
+// 1 ≤ s2.length ≤ 15.
+
+// [output] integer
+
 function commonCharacterCount(s1, s2) {
     s1 = s1.split("");
     console.log(s1);
