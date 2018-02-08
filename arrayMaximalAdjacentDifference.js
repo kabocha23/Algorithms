435e8dc9f9e03cb71ef089b12fed8d5f8b144e93
kabocha23 <jekobuchi@gmail.com>
2018-02-08 09:42:29 -0800

    CodeFights array Maximal Adjacent Difference

---
+++ arrayMaximalAdjacentDifference.js
@@ -0,0 +1,30 @@
+// Given an array of integers, find the maximal absolute difference between any two of its adjacent elements.
+
+// Example
+
+// For inputArray = [2, 4, 1, 0], the output should be
+// arrayMaximalAdjacentDifference(inputArray) = 3.
+
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] array.integer inputArray
+
+// Guaranteed constraints:
+// 3 ≤ inputArray.length ≤ 10,
+// -15 ≤ inputArray[i] ≤ 15.
+
+// [output] integer
+
+// The maximal absolute difference.
+
+function arrayMaximalAdjacentDifference(inputArray) {
+    let max = Math.abs(inputArray[0] - inputArray[1]);
+    
+    for(let i = 1; i < inputArray.length; i++){
+        let currentMax = Math.abs(inputArray[i-1] - inputArray[i])
+        max = (currentMax > max) ? currentMax : max;
+    }
+    return max;
+}
