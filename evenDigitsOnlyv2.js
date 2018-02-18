cf7512bb183b3e5f4c42b0b754653d780f469567
kabocha23 <jekobuchi@gmail.com>
2018-02-18 15:43:13 -0800

    CodeFights evenDigitsOnly, two solutions

---
+++ evenDigitsOnlyv2.js
@@ -0,0 +1,24 @@
+// Check if all digits of the given integer are even.
+
+// Example
+
+// For n = 248622, the output should be
+// evenDigitsOnly(n) = true;
+// For n = 642386, the output should be
+// evenDigitsOnly(n) = false.
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] integer n
+
+// Guaranteed constraints:
+// 1 ≤ n ≤ 109.
+
+// [output] boolean
+
+// true if all digits of n are even, false otherwise.
+
+function evenDigitsOnly(n) {
+    return (n+"").split("").every(x=>x%2===0)
+}
