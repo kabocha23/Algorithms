b851a4018649bd0865c8602b88822341733d7cd2
kabocha23 <jekobuchi@gmail.com>
2018-02-19 12:36:42 -0800

    CodeFights circleOfNumbers

---
+++ circleOfNumbers.js
@@ -0,0 +1,34 @@
+// Consider integer numbers from 0 to n - 1 written down along the circle in such a way that the distance between any two neighbouring numbers is equal (note that 0 and n - 1 are neighbouring, too).
+
+// Given n and firstNumber, find the number which is written in the radially opposite position to firstNumber.
+
+// Example
+// For n = 10 and firstNumber = 2, the output should be
+// circleOfNumbers(n, firstNumber) = 7.
+
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] integer n
+
+// A positive even integer.
+
+// Guaranteed constraints:
+// 4 ≤ n ≤ 20.
+
+// [input] integer firstNumber
+
+// Guaranteed constraints:
+// 0 ≤ firstNumber ≤ n - 1.
+
+// [output] integer
+
+function circleOfNumbers(n, firstNumber) {
+    if(firstNumber < (n/2)){
+        return ((n/2) + firstNumber);
+    }else{
+        return (firstNumber - (n/2));
+    }
+    
+}
