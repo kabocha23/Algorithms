fe06d18bf5d425e42c095a024e97160aee74de2f
kabocha23 <jekobuchi@gmail.com>
2018-02-27 13:43:25 -0800

    Edited prompt

--- shapeArea.js
+++ shapeArea.js
@@ -2,8 +2,6 @@
 
 // A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by taking the n - 1-interesting polygon and appending 1-interesting polygons to its rim, side by side. You can see the 1-, 2-, 3- and 4-interesting polygons in the picture below.
 
-
-
 // Example
 
 // For n = 2, the output should be
