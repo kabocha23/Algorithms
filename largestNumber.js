9e33df74bcfe63f532c2638c07da8c5f77470e3f
kabocha23 <jekobuchi@gmail.com>
2018-03-05 11:50:09 -0800

    CodeFights largestNumber

---
+++ largestNumber.js
@@ -0,0 +1,23 @@
+// Given an integer n, return the largest number that contains exactly n digits.
+
+// Example
+// For n = 2, the output should be
+// largestNumber(n) = 99.
+
+// Input/Output
+// [execution time limit] 4 seconds (js)
+
+// [input] integer n
+// Guaranteed constraints:
+// 1 ≤ n ≤ 9.
+
+// [output] integer
+// The largest integer of length n.
+
+function largestNumber(n) {
+    let str = '';
+    while(str.length < n){
+        str += 9;
+    }
+    return parseInt(str);
+}
