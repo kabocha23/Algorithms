6b9dc36b0192e94e1ea970c37c2d5b069c1487e0
kabocha23 <jekobuchi@gmail.com>
2018-03-16 13:56:25 -0700

    CodeFights isDigit

---
+++ isDigit.js
@@ -0,0 +1,23 @@
+// Determine if the given character is a digit or not.
+
+// Example
+
+// For symbol = '0', the output should be
+// isDigit(symbol) = true;
+// For symbol = '-', the output should be
+// isDigit(symbol) = false.
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] char symbol
+
+// A character which is either a digit or not.
+
+// [output] boolean
+
+// true if symbol is a digit, false otherwise.
+
+function isDigit(symbol) {
+    return !isNaN(parseInt(symbol, 10));
+}
