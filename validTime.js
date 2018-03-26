9e1cea9ea14f8e4bb14741eb45178184e7fb09d7
kabocha23 <jekobuchi@gmail.com>
2018-03-26 10:25:47 -0700

    CodeFights validTime

---
+++ validTime.js
@@ -0,0 +1,26 @@
+// Check if the given string is a correct time representation of the 24-hour clock.
+
+// Example
+
+// For time = "13:58", the output should be
+// validTime(time) = true;
+// For time = "25:51", the output should be
+// validTime(time) = false;
+// For time = "02:76", the output should be
+// validTime(time) = false.
+// Input/Output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] string time
+
+// A string representing time in HH:MM format. It is guaranteed that the first two characters, as well as the last two characters, are digits.
+
+// [output] boolean
+
+// true if the given representation is correct, false otherwise.
+
+
+function validTime(time) {
+    return /^([01]\d|2[0-3]):?([0-5]\d)$/i.test(time);
+}
