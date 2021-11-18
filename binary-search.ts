5c9b1b2f14696e796815be84e7056b2eb275374e
Jason Kobuchi <jekobuchi@gmail.com>
2021-11-18 11:32:15 -0800

    moved js files to new folder

---
+++ binary-search.ts
@@ -0,0 +1,19 @@
+function binarySearch(nums: number[], target: number): number {
+    let start: number = 0;
+    let end: number = nums.length-1
+    
+    while (start <= end){
+        let mid: number = Math.floor((start+end)/2);
+        
+        if(target === nums[mid]) {
+            return mid;
+        }
+        if(target > nums[mid]){
+            start = mid+1;
+        }
+        if(target < nums[mid]){
+            end = mid+1;
+        }
+    }
+    return -1;
+};
