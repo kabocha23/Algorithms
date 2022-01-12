4ce2ffd180f305024821e23f4f4e76d7ce9fb53e
Jason Kobuchi <jekobuchi@gmail.com>
2022-01-11 21:04:13 -0800

    merge two sorted lists ts algo added

---
+++ merge-two-sorted-lists.ts
@@ -0,0 +1,27 @@
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    let list3: ListNode = new ListNode();
+    let current = list3;
+    
+    while(list1 && list2) {
+        if(list1.val < list2.val) {
+            current.next = list1
+            list1 = list1.next
+        } else {
+            current.next = list2
+            list2 = list2.next
+        }
+        current = current.next
+        console.log(list3.next)
+    }
+    current.next = list1 || list2
+     return list3.next;
+};
