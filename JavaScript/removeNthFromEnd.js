d184949275cdd9dc171648c5dacd0be519ebebd4
Jason Kobuchi <jekobuchi@gmail.com>
2022-10-04 17:02:50 -0700

    removeNthFromEnd added description

--- JavaScript/removeNthFromEnd.js
+++ JavaScript/removeNthFromEnd.js
@@ -12,6 +12,12 @@
 // Input: head = [1,2], n = 1
 // Output: [1]
 
+// create two pointers, one pointer will signal the end of the list, 
+// and the other pointer will be the nth from the first pointer (node to remove)
+// set the first pointer to next until it has a distance of n from the second pointer
+// move all pointers forwards until the first pointer reaches the end
+// the second pointer will be at the nth from the last position, so it will be removed
+
 const removeNthFromEnd = (head, n) => {
     let end = head
     let nth = head;
