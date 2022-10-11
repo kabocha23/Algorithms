4515718c1f2fa37cee887de0d7b725c5ac6fa399
Jason Kobuchi <jekobuchi@gmail.com>
2022-10-10 18:21:44 -0700

    wordBreak added

---
+++ JavaScript/wordBreak.js
@@ -0,0 +1,36 @@
+// Leetcode 139. Word Break
+// Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+// Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+// Example 1:
+// Input: s = "leetcode", wordDict = ["leet","code"]
+// Output: true
+// Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+// Example 2:
+// Input: s = "applepenapple", wordDict = ["apple","pen"]
+// Output: true
+// Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+// Note that you are allowed to reuse a dictionary word.
+
+// Example 3:
+// Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+// Output: false
+
+const wordBreak = (s, wordDict) => {
+    const table = new Array(s.length + 1).fill(false);
+    
+    table[0] = true;
+
+    for(let i = 0; i < table.length; i++) {
+        if(table[i] === false) continue;
+        for(let j = i+1; j < table.length; j++) {
+            console.log(s.slice(i,j))
+            if(wordDict.includes(s.slice(i,j))) {
+                table[j] = true;
+            }
+        }
+    }
+
+    return table[table.length-1]
+};
