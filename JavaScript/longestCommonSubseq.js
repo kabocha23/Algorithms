0d17ec353392fba4a4f456f7dacf2496b1f9364a
Jason Kobuchi <jekobuchi@gmail.com>
2022-10-11 22:01:17 -0700

    longestCommonSubseq explanation added

--- JavaScript/longestCommonSubseq.js
+++ JavaScript/longestCommonSubseq.js
@@ -20,6 +20,13 @@
 // Explanation: There is no such common subsequence, so the result is 0.
 
 
+// create a table (2d matrix) to track the comparison of each string's characters
+// the first row and column of the table will be 0's to represent the start of a new subsequence
+// iterate through text1 and text2 comparing chars, if the chars are equal, add 1 to the previous diagonal table element
+// if not, take the max of the top and left elements of the table
+// adding 1 to the diagonal element will keep track of the longest common subsquence
+// return the last element of the matrix
+
 const longestCommonSubsequence = (text1, text2) => {
     let table = Array.from(Array(text1.length+1), () => new Array(text2.length+1).fill(0))
     
