a1ef7b68654a0f8ba570fadd4253b0e8742e1bbc
Jason Kobuchi <jekobuchi@gmail.com>
2022-10-31 11:37:09 -0700

    added explanation

--- TypeScript/mergeTwoLists.ts
+++ TypeScript/mergeTwoLists.ts
@@ -1,3 +1,14 @@
+
+// create a new ListNode for the merged list
+// initialize a pointer to the new list
+// iterate through both lists, comparing values
+// if list1's val is less than list2's, set list3 pointer's next to list1
+// and move list1 to list1.next
+// if list2's val is smaller, do it for list2
+// increment list3's pointer to next node
+// finally check if list1 or list2 still have nodes, indicating uneven lists
+// have list3 pointer point to the remaing nodes 
+
 class ListNode {
     val: number
     next: ListNode | null
@@ -5,9 +16,9 @@ class ListNode {
         this.val = (val===undefined ? 0 : val)
         this.next = (next===undefined ? null : next)
     }
-}
+};
 
-function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+const mergeTwoLists = (list1: ListNode | null, list2: ListNode | null): ListNode | null => {
     let list3: ListNode = new ListNode();
     let current = list3;
     
@@ -20,7 +31,6 @@ function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode
             list2 = list2.next
         }
         current = current.next
-        console.log(list3.next)
     }
     current.next = list1 || list2
      return list3.next;
